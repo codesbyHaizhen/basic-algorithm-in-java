7d333f2796f3561089384dbc41a26da1b9545d11
codesbyHaizhen <angelblanca1@hotmail.com>
2021-12-20 17:11:27 +0100

    to resolve the leetcode problem 1: twoSum

---
+++ src/com/example/binarysearch/TwoSum.java
@@ -0,0 +1,40 @@
+package com.example.binarysearch;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
+public class TwoSum {
+	
+	// to solve the leetcode problem 1: with HashMap
+	public int[] getTwoSum(int[] nums, int target) {
+		
+		Map<Integer,Integer> map = new HashMap<>();
+		int[] results = new int[2];
+		
+		for (int i=0; i<nums.length; i++) {
+			
+			if (map.containsKey(nums[i])) {
+				results[0] = map.get(nums[i]);
+				results[1] = i;
+				return results;
+			}
+			
+			map.put(target-nums[i], i);
+		}
+		
+		return results;
+		
+	}
+	
+	public static void main(String... args) {
+		
+		TwoSum twoSum = new TwoSum();
+		int[] nums = {1,3,5,6,8,9,10,34,45,56,67,68,69,74,99,109,120};
+		int target = 48;
+		
+		int[] results = twoSum.getTwoSum(nums, target);
+		System.out.println(Arrays.toString(results));
+	}
+
+}
