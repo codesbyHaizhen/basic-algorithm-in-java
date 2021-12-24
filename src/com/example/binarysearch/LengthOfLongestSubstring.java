b9499bded49cf0c12f422066787425659ed728dd
codesbyHaizhen <angelblanca1@hotmail.com>
2021-12-24 09:21:07 +0100

    with HashMap to resolve the leetcode problem 3: find out the length of the longest substring

---
+++ src/com/example/binarysearch/LengthOfLongestSubstring.java
@@ -0,0 +1,37 @@
+package com.example.binarysearch;
+
+import java.util.HashMap;
+
+public class LengthOfLongestSubstring {
+	
+	public static void main(String[] args) {
+		
+		String str = "abvfghiaopbg";
+		
+		LengthOfLongestSubstring lols = new LengthOfLongestSubstring();
+		int len = lols.getLengthOfLongestSubstring(str);
+		
+		System.out.println(len);
+	}
+	
+	// leetcode 3: given a String, return the length of the longest substring without repeating character
+	private int getLengthOfLongestSubstring (String str) {
+		
+		HashMap<Character, Integer> map = new HashMap<>();
+		int start = 0;
+		int len = 0;
+		int lengthOfStr = str.length();
+		
+		for (int i=0; i<lengthOfStr; i++) {
+			char tempChar = str.charAt(i);
+			
+			if (map.containsKey(tempChar)) {
+				start = Math.max(start, map.get(tempChar)+1);
+			}
+			
+			map.put(tempChar, i);
+			len = Math.max(len, i+1-start);
+		}
+		return len;
+	}
+}
