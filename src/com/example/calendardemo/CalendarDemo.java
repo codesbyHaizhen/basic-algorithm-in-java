5046b6be230f376547acf44d5838a92b240a57bc
codesbyHaizhen <angelblanca1@hotmail.com>
2022-03-08 17:32:25 +0100

    Refactoring nach EAV Prinzip

--- src/com/example/calendardemo/CalendarDemo.java
+++ src/com/example/calendardemo/CalendarDemo.java
@@ -7,24 +7,46 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import static com.example.iodemo.FileOperationUseBuffer.writeToFile;
 
-import com.example.iodemo.FileOperationUseBuffer;
+/**
+ * Class for get a month calendar from the inputed date and save it in a text file
+ * 
+ * @author haizhen
+ *
+ */
 
 public class CalendarDemo {
 	
+	private Scanner scanner = new Scanner(System.in);
+	private ArrayList<String> list = new ArrayList<>();
+	private String inputStr;
+	
 	public static void main(String[] args) throws IOException {
+		new CalendarDemo();
+	}
+
+	private CalendarDemo() throws IOException {
+		input();
+		process();
+		output();
+	}
+	
+	private void input() {
 		System.out.println("Please input a date with Format dd.mm.yyyy ");
-		Scanner scanner = new Scanner(System.in);
-		String inputStr = scanner.next();
 		
-		ArrayList<String> list = new ArrayList<>();
-		
+		inputStr = scanner.next();
+		scanner.close();
+	}
+	
+	private void output() throws IOException {
+		writeToFile(list, "Calendar.txt");
+	}
+	
+	private void process() {
 		list.add("Mo.\tDi.\tMi.\tDo.\tFr.\tSa.\tSo.\t");
-		System.out.println("Mo.\tDi.\tMi.\tDo.\tFr.\tSa.\tSo.\t");
 		
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 		LocalDate date = LocalDate.parse(inputStr,formatter);
 		
-		int day = date.getDayOfMonth();
 		date = LocalDate.of(date.getYear(), date.getMonth(), 1);
 		int weekDay = date.getDayOfWeek().getValue();
 		int len = date.lengthOfMonth();
@@ -32,28 +54,19 @@ public class CalendarDemo {
 		StringBuilder temp = new StringBuilder();
 		
 		for (int i=1;i<weekDay;i++) {
-			System.out.print("\t");
 			temp = temp.append("\t");
 			count++;
 		}
 		for (int j=1;j<=len;j++) {
-			System.out.print(j+"\t");
 			temp = temp.append(j+"\t");
 			count++;
 			if(count%7==0) {
 				list.add(temp.toString());
 				temp = temp.delete(0, temp.length());
-				System.out.println();
 			}
 		}
 		if (temp.length()!=0) {
 			list.add(temp.toString());
 		}
-		System.out.println(list);
-		
-		writeToFile(list, "Calendar.txt");
-
 	}
-	
-
 }
